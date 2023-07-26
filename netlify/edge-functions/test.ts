b9975f12d3da4a06974a3c8e89a9c45a01651341
Matt Kane <m@mk.gg>
2023-07-26 09:31:51 +0100

    Add log

--- netlify/edge-functions/test.ts
+++ netlify/edge-functions/test.ts
@@ -1,13 +1,9 @@
 import { Config, Context } from "https://deploy-preview-243--edge.netlify.app";
 import { Blobs } from "https://blobs-js.netlify.app/main.ts";
 
-export interface Manifest {
-  chunks: Array<{ sequence: number; duration: number }>;
-  targetDuration: number;
-}
 export default async function handler(request: Request, context: Context) {
   const blobs: Blobs = context.blobs;
-
+  console.log(request);
   if (request.method === "PUT") {
     if (!request.body) {
       return new Response("No body", { status: 400 });
