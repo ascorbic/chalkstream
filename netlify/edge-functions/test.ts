bbc0ba1eb185fa8ff0be6b939f5729c3a8622ba4
Matt Kane <m@mk.gg>
2023-07-26 21:05:13 +0100

    Use new blobs

--- netlify/edge-functions/test.ts
+++ netlify/edge-functions/test.ts
@@ -1,14 +1,16 @@
 import { Config, Context } from "https://deploy-preview-243--edge.netlify.app";
-import { Blobs } from "https://blobs-js.netlify.app/main.ts";
+import { Blobs } from "https://deploy-preview-25--blobs-js.netlify.app/main.ts";
 
 export default async function handler(request: Request, context: Context) {
-  const blobs: Blobs = context.blobs;
+  const blobs = new Blobs(context.blobs);
   console.log(request);
   if (request.method === "PUT") {
     if (!request.body) {
       return new Response("No body", { status: 400 });
     }
-    await blobs.set("test", request.body);
+    await blobs.set("test", request.body, {
+      contentLength: Number(request.headers.get("content-length")),
+    });
     return new Response("OK", { status: 200 });
   }
 
