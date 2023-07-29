47d1c138b82300f960f5344d1c8aa59399bf2ae2
Matt Kane <m@mk.gg>
2023-07-29 16:16:02 +0100

    Add url parsing

--- src/play.ts
+++ src/play.ts
@@ -1,13 +1,36 @@
 import Hls from "hls.js";
 
-if (Hls.isSupported()) {
-  const video = document.getElementById("video") as HTMLMediaElement;
-  const hls = new Hls();
-  // bind them together
-  hls.attachMedia(video);
-  // MEDIA_ATTACHED event is fired by hls object once MediaSource is ready
-  hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-    console.log("video and hls.js are now bound together !");
-  });
-  hls.loadSource("/manifest/session-id.m3u8");
+async function load() {
+  if (!("URLPattern" in globalThis)) {
+    await import("urlpattern-polyfill");
+  }
+
+  if (Hls.isSupported()) {
+    const video = document.getElementById("video") as HTMLMediaElement;
+    const hls = new Hls();
+    // bind them together
+
+    const pattern = new URLPattern({
+      pathname: "/play/:sessionid",
+    });
+    const result = pattern.exec(window.location.pathname);
+    const { sessionid } = result?.pathname.groups ?? {};
+    if (!sessionid) {
+      console.log("no sessionid");
+    } else {
+      console.log(`getting ${sessionid}.m3u8`);
+      hls.loadSource(`/manifest/${sessionid}.m3u8`);
+      hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+        console.log(
+          "manifest loaded, found " + data.levels.length + " quality level"
+        );
+      });
+      hls.attachMedia(video);
+      // MEDIA_ATTACHED event is fired by hls object once MediaSource is ready
+      hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+        console.log("video and hls.js are now bound together !");
+      });
+    }
+  }
 }
+load();
