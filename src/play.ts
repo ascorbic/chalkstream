1c47bb31a7556b851df30062fe2da9716e2439c3
Matt Kane <m@mk.gg>
2023-07-29 21:57:13 +0100

    Use video.js

--- src/play.ts
+++ src/play.ts
@@ -1,20 +1,15 @@
-import Hls from "hls.js";
-
-const video = document.getElementById("video") as HTMLMediaElement;
-
 async function load() {
   if (!("URLPattern" in globalThis)) {
     await import("urlpattern-polyfill");
   }
-
+  const player = globalThis.videojs?.("video");
+  if (!player) {
+    console.log("no player");
+    return;
+  }
   const pattern = new URLPattern({
     pathname: "/play/:sessionid",
   });
-
-  if (!Hls.isSupported()) {
-    console.log("No HLS support. Try a newer browser.");
-    return;
-  }
 
   const result = pattern.exec(window.location.href);
 
@@ -25,8 +20,9 @@ async function load() {
     return;
   }
 
-  const hls = new Hls();
-  hls.loadSource(`/playlist/${sessionid}.m3u8`);
-  hls.attachMedia(video);
+  player.src({
+    src: `/playlist/${sessionid}.m3u8`,
+    type: "application/x-mpegURL",
+  });
 }
 load();
