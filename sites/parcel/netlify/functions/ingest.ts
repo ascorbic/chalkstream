356497452efc5945f8cea6440545d5c089a0dae4
Matt Kane <ascorbic@gmail.com>
2023-11-16 21:29:39 +0000

    fix: correct exports and files (#8)

--- sites/parcel/netlify/functions/ingest.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "@netlify/functions";
-import { ingestHandler } from "chalkstream/node";
-
-export default ingestHandler;
-
-export const config: Config = {
-  method: "PUT",
-  path: "/ingest/:session/:digest.ts",
-};
